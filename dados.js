bc6cca7739771cd9e6b241caa4cabbda847383f2
Destroyer-BR1 <bernardoalvao@gmail.com>
2024-09-06 16:13:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Energia Solar",
+        descricao: "A energia solar é uma fonte de energia renovável obtida a partir da luz e do calor do Sol. Essa energia é captada por painéis solares e convertida em eletricidade, que pode ser utilizada para alimentar residências, empresas e até mesmo redes elétricas.",
+        link: "https://pt.wikipedia.org/wiki/wiki/Energia_solar"
+    },
+    {
+        titulo: "Energia Eólica",
+        descricao: "A energia eólica é a energia obtida a partir do vento. Grandes hélices, chamadas de aerogeradores, captam a força do vento e a transformam em energia elétrica.",
+        link: "https://pt.wikipedia.org/wiki/Energia_eólica"
+    },
+    {
+        titulo: "Energia da Biomassa",
+        descricao: "A energia da biomassa é obtida a partir da queima de matéria orgânica, como madeira, resíduos agrícolas e de animais. Essa queima gera calor, que pode ser utilizado para gerar eletricidade ou aquecer ambientes.",
+        link: "https://pt.wikipedia.org/wiki/Biomassa"
+    }
+];
